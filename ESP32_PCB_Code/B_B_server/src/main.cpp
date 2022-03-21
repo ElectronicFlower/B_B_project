41c7609ba41a8bdc9d92fce60dac2ce920462544
Lucia <90812820+ElectronicFlower@users.noreply.github.com>
2022-03-21 19:11:27 -0400

    Deep Sleep Function was added

--- ESP32_PCB_Code/B_B_server/src/main.cpp
+++ ESP32_PCB_Code/B_B_server/src/main.cpp
@@ -16,6 +16,13 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
+//Time definition for Deep Sleep
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+
+//RTC Storage
+RTC_DATA_ATTR int bootCount = 0;
+
 //Default Temperature is in Celsius
 //Comment the next line for Temperature in Fahrenheit
 #define temperatureCelsius
@@ -31,6 +38,7 @@ DHT dht(DHTPIN, DHTTYPE);
 
 float temp;
 float tempF;
+int i;
 
 // Timer variables
 unsigned long lastTime = 0;
@@ -62,6 +70,22 @@ class MyServerCallbacks: public BLEServerCallbacks {
     //u8x8.drawString(0,3,"Disconnected");
   }
 };
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
+  }
+}
 
 void setup() {
 
@@ -99,38 +123,61 @@ void setup() {
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
-}
 
-void loop() {
-  if (deviceConnected) {
-    if ((millis() - lastTime) > timerDelay) {
-      // Read temperature as Celsius (the default)
-      temp = dht.readTemperature();
-      // Fahrenheit
-      tempF = dht.readTemperature(false);
-  
-      //Notify temperature reading from BME sensor
-      #ifdef temperatureCelsius
-        static char temperatureCTemp[6];
-        //Set temperature Characteristic value and notify connected client
-        dtostrf(temp, 6, 2, temperatureCTemp);
-        bmeTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
-        bmeTemperatureCelsiusCharacteristics.notify();
-        Serial.print("Temperature Celsius: ");
-        Serial.print(temp); 
-        Serial.print(" ºC\n");
-      #else
-        static char temperatureFTemp[6];
-        dtostrf(tempF, 6, 2, temperatureFTemp);
-        //Set temperature Characteristic value and notify connected client
-        bmeTemperatureFahrenheitCharacteristics.setValue(temperatureFTemp);
-        bmeTemperatureFahrenheitCharacteristics.notify();
-        Serial.print("Temperature Fahrenheit: ");
-        Serial.print(tempF);
-        Serial.print(" ºF");
-      #endif
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
 
-      lastTime = millis();
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  //First we configure the wake up source We set our ESP32 to wake up every 5 seconds
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+
+  //Main code that will repeat after start up and will repeat 5 times, after completed will sleep
+  for (i=0; i<30; ++i){
+    if (deviceConnected) {
+      if ((millis() - lastTime) > timerDelay) {
+        // Read temperature as Celsius (the default)
+        temp = dht.readTemperature();
+        // Fahrenheit
+        tempF = dht.readTemperature(false);
+    
+        //Notify temperature reading from BME sensor
+        #ifdef temperatureCelsius
+          static char temperatureCTemp[6];
+          //Set temperature Characteristic value and notify connected client
+          dtostrf(temp, 6, 2, temperatureCTemp);
+          bmeTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
+          bmeTemperatureCelsiusCharacteristics.notify();
+          Serial.print("Temperature Celsius: ");
+          Serial.print(temp); 
+          Serial.print(" ºC\n");
+        #else
+          static char temperatureFTemp[6];
+          dtostrf(tempF, 6, 2, temperatureFTemp);
+          //Set temperature Characteristic value and notify connected client
+          bmeTemperatureFahrenheitCharacteristics.setValue(temperatureFTemp);
+          bmeTemperatureFahrenheitCharacteristics.notify();
+          Serial.print("Temperature Fahrenheit: ");
+          Serial.print(tempF);
+          Serial.print(" ºF");
+        #endif
+
+        lastTime = millis();
+      }
     }
   }
+
+  //Sleep process begins
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+}
+
+void loop(){ 
 }
