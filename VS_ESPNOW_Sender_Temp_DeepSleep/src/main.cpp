924448874b992cd4d108192418f81c100bd156e7
Lucia <90812820+ElectronicFlower@users.noreply.github.com>
2022-04-16 13:47:10 -0400

    Update main.cpp

--- VS_ESPNOW_Sender_Temp_DeepSleep/src/main.cpp
+++ VS_ESPNOW_Sender_Temp_DeepSleep/src/main.cpp
@@ -1,31 +1,17 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-two-way-communication-esp32/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
 // SENDER
 #include <esp_now.h>
 #include <WiFi.h>
-
 #include <Wire.h>
 #include <DHT.h>
 
-//#define SCREEN_WIDTH 128  // OLED display width, in pixels
-//#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+//DHT definitions
 #define DHTTYPE DHT22
 #define DHTPIN 4
-
-#define SLEEP_SECS 5 // 5 secs
-#define SEND_TIMEOUT 245  // 245 millis seconds timeout 
-
 DHT dht(DHTPIN, DHTTYPE);
 
+//Deep
+#define SLEEP_SECS 5 // 5 secs
+#define SEND_TIMEOUT 245  // 245 millis seconds timeout 
 
 // REPLACE WITH THE MAC Address of your receiver 
 uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x5B, 0x33, 0x20};
@@ -33,8 +19,6 @@ uint8_t broadcastAddress[] = {0x7C, 0x9E, 0xBD, 0x5B, 0x33, 0x20};
 // Define variables to store DHT readings to be sent
 float temperature;
 float humidity;
-// Define variables to store incoming readings
-
 
 // Variable to store if sending data was successful
 String success;
@@ -68,75 +52,6 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   }
 }
 
-// Callback when data is received
-
- 
-void setup() {
-  // Init Serial Monitor
-  Serial.begin(115200);
-  Serial.println(F("DHTxx test!"));
-  // Init DHT sensor
-  dht.begin();
-
-  // Init OLED display
-
- 
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-  //Deep sleep
-  WiFi.disconnect();
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    gotoSleep(); //Deep Sleep
-  }
-
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
-  
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
-  // Register for a callback function that will be called when data is received
-  //esp_now_register_recv_cb(OnDataRecv);
-}
- 
-void loop() {
-  //Go to sleep
-  if (callbackCalled || (millis() > SEND_TIMEOUT)) {
-    gotoSleep();
-  }
-  // Wait a few seconds between measurements.
-  delay(2000);
-  getReadings();
- 
-  // Set values to send
-  dhtReadings.temp=temperature;
-  dhtReadings.hum=humidity;
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &dhtReadings, sizeof(dhtReadings));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
-  }
-  else {
-    Serial.println("Error sending the data");
-  }
-  updateDisplay();
-  delay(10000);
-  }
-  
 void getReadings(){
   humidity = dht.readHumidity();
   // Read temperature as Celsius (the default)
@@ -168,3 +83,72 @@ void gotoSleep(){
   Serial.printf("Up for %i ms, going to sleep for %i secs...\n", millis(), SLEEP_SECS); 
   ESP.deepSleep(SLEEP_SECS * 1000000);
 }
+ 
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+  Serial.println(F("DHTxx test!"));
+  // Init DHT sensor
+  dht.begin();
+  //Setup of the LED light on pcb
+  pinMode(pinLED, OUTPUT);
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  //Deep sleep
+  WiFi.disconnect();
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    gotoSleep(); //Deep Sleep
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  //esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  //led will toggle whenever we are within the loop
+  led_toggle = led_toggle == 0 ? 1 : 0;
+  digitalWrite(22, led_toggle);
+  
+  //Go to sleep
+  if (callbackCalled || (millis() > SEND_TIMEOUT)) {
+    gotoSleep();
+  }
+  // Wait a few seconds between measurements.
+  delay(2000);
+  getReadings();
+ 
+  // Set values to send
+  dhtReadings.temp=temperature;
+  dhtReadings.hum=humidity;
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &dhtReadings, sizeof(dhtReadings));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  updateDisplay();
+  delay(10000);
+  }
+
